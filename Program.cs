9d5566ddb26757d5dc353eb3687c84e9f3c5d145
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-08-26 18:53:28 -0300

    ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+try
+{
+    Console.WriteLine("Ingrese la base:");
+    Double num = Convert.ToDouble(Console.ReadLine());
+
+    Double cuadrado = num * num;
+
+
+    Console.WriteLine("Cuadrado:" + cuadrado);
+    Console.ReadKey();
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Valor invalido: " + e.Message);
+}
