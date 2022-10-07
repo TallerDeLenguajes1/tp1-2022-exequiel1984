ac0e649f2b6aa2e566d9f39962ab21ad81fdfa12
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-07 13:29:03 -0300

    Agrego funcion y los 3 catch aritmeticos

--- Program.cs
+++ Program.cs
@@ -1,25 +1,54 @@
-﻿
+﻿Console.WriteLine("---CALCULO DE KILOMETROS POR LITRO---");
 
-try
+int kilometrosPorLitro = CalcularKilometrosPorLitros();
+
+Console.WriteLine("Kilometros por litro: " + kilometrosPorLitro);
+
+Console.WriteLine("FIN");
+
+
+static int CalcularKilometrosPorLitros()
 {
-    Console.WriteLine("Ingrese los kilometros:");
-    int kilometros = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Ingrese los litros:");
-    int litros = Convert.ToInt32(Console.ReadLine());
-
-
-
-    int kilometrosPorLitro = kilometros / litros;
-
-
-    Console.WriteLine("Kilometros por litro:" + kilometrosPorLitro);
-
-}
-catch (DivideByZeroException e)
-{
-    Console.WriteLine("No se puede dividir en 0 " + e.Message);
-}
-catch (FormatException e)
-{
-    Console.WriteLine("metiste una letra " + e.Message);
+    try
+    {
+       Console.WriteLine("Ingrese los kilometros:");
+        int kilometros = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese los litros:");
+        int litros = Convert.ToInt32(Console.ReadLine());
+        return kilometros / litros; 
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("No se puede dividir en 0");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    catch(FormatException ex)
+    {
+        Console.WriteLine("Ingreso un valor invalido");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    catch(OverflowException ex)
+    {
+        Console.WriteLine("Ingreso un numero demasiado grande ");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    finally
+    {
+        Console.WriteLine("GAME OVER");
+    }
 }
