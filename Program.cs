6ada4cecc300f57c40b3947a85d9430124aed494
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-06 22:08:15 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,17 +1,1 @@
 ﻿
-
-try
-{
-    Console.WriteLine("Ingrese la base:");
-    Double num = Convert.ToDouble(Console.ReadLine());
-
-    Double cuadrado = num * num;
-
-
-    Console.WriteLine("Cuadrado:" + cuadrado);
-    Console.ReadKey();
-}
-catch (FormatException e)
-{
-    Console.WriteLine("Valor invalido: " + e.Message);
-}
