4b7420c6a9a1e0e1029571a173f3b78f187f4919
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-07 12:03:02 -0300

    agrego branch Problema1 para solucionar problema de branch master y Punto1-Problema1

--- Program.cs
+++ Program.cs
@@ -1,21 +1,39 @@
-﻿
+﻿int cuadrado = CalculoCuadrado();
+Console.WriteLine($"El cuadrado es {cuadrado}");
 
-try
+
+
+static int CalculoCuadrado()
 {
-    Console.WriteLine("Ingrese el dividendo:");
-    int dividendo = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Ingrese el divisor:");
-    int divisor = Convert.ToInt32(Console.ReadLine());
-
-
-
-    int cociente = dividendo / divisor;
-
-
-    Console.WriteLine("Resultado:" + cociente);
-
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
+    
+    try
+    {
+        Console.WriteLine("Ingrese un numero entero");
+        int numero = Int32.Parse(Console.ReadLine());
+        return numero * numero;   
+    }
+    catch(FormatException ex)
+    {
+        Console.WriteLine("Ingreso un valor invalido");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    catch(OverflowException ex)
+    {
+        Console.WriteLine("Ingreso un numero demasiado grande ");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    finally
+    {
+        Console.WriteLine("GAME OVER");
+    }
 }
