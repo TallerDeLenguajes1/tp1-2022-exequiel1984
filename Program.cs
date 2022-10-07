f91e511a1b17377349d6289e2778430fba4508d5
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-07 13:07:17 -0300

    Realizo el try con el DivideByZero

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
-﻿
+﻿Console.WriteLine("---CALCULO DE DIVISION---");
 
-try
+int cociente = CalcularDivision();
+
+Console.WriteLine("Resultado = " + cociente);
+
+static int CalcularDivision()
 {
-    Console.WriteLine("Ingrese el dividendo:");
-    int dividendo = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Ingrese el divisor:");
-    int divisor = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        Console.WriteLine("Ingrese el dividendo:");
+        int dividendo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el divisor:");
+        int divisor = Convert.ToInt32(Console.ReadLine());
 
-
-
-    int cociente = dividendo / divisor;
-
-
-    Console.WriteLine("Resultado:" + cociente);
-
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
+        int resultado = dividendo / divisor;
+        return resultado;
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine("Ingreso 0 en el divisor. No es posible dividir en 0");
+        Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+        Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+        Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+        Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+        Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+        throw;
+    }
+    
 }
