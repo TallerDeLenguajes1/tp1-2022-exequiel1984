c0c2b7bc9b77933ea0f2f6598790bc6ee72b09cc
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-10-10 13:51:10 -0300

    trabajando en la carga de lista

--- Program.cs
+++ Program.cs
@@ -6,16 +6,59 @@
         {
             List<Empleado> listaEmpleados = new List<Empleado>();
 
+            CargarListaEmpleados(listaEmpleados);
+
+            MostrarListaEmpleados(listaEmpleados);
+        }
+
+        private static void CargarListaEmpleados(List<Empleado> listaEmpleados)
+        {
+
             Console.WriteLine("Ingrese la cantidad de empleados a cargar:");
 
             try
             {
-                int CantidadEmpleadosACargar = Convert.ToInt32(Console.ReadLine());         
+                int CantidadEmpleadosACargar = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < CantidadEmpleadosACargar; i++)
+                {
+                    Empleado NuevoEmpleado = new Empleado();
+
+                    Console.WriteLine("Ingrese el apellido del empleado: ");
+                    NuevoEmpleado.DataPersonal.Apellido = Console.ReadLine();
+
+
+            
+                    listaEmpleados.add(NuevoEmpleado);
+                }
             }
-            catch (FormatException e)
+            catch (FormatException ex)
             {
                 Console.WriteLine("Ingreso un valor invalido ");
+                Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+                Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+                Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+                Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+                Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
                 throw;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Ingreso un numero demasiado grande");
+                Console.WriteLine($"Mensaje para el usuario: {ex.Message}");
+                Console.WriteLine($"Nombre de la fuente del error: {ex.Source}");
+                Console.WriteLine($"Pila de llamadas: {ex.StackTrace}");
+                Console.WriteLine($"Nombre que lanza la excepcion: {ex.TargetSite}");
+                Console.WriteLine($"Diccionario con info adicional: {ex.Data}");
+            }
+
+            
+        }
+
+        private static void MostrarListaEmpleados(List<Empleado> listaEmpleados)
+        {
+            foreach (Emple item in listaEmpleados)
+            {
+                Console.WriteLine("Apellido: " + Emple.DataPersonal.Apellido);
             }
         }
     }
